02323349c2ccb4534298143f7ebf9920839d8a8c
Orlando Mauro <orlandomauro2000@gmail.com>
2023-03-16 23:56:27 +0100

    sexto commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,6 +2,17 @@
 
 int main()
 {
+    int variable = 10, memory, *punt;
+    punt = &variable;
     printf("Hola mundo\n");
+
+    memory = sizeof(variable);
+
+    printf("La direccion de memoria almacenada en el puntero es: %d\n", punt);
+    printf("La direccion de memoria de la variable es: %d\n", &variable);
+    printf("La direccion de memoria del puntero es: %d\n", &punt);
+    printf("La cantidad de memoria usada por la variable es: %d\n", memory);
+    printf("El contenido del puntero es: %d\n", *punt);
+
     return 0;
 }
